05bd8f2d8f3d8f2e5804b3cf8c9829bba0ea9c2e
katoy <youichikato@gmail.com>
2020-01-28 02:54:27 +0900

    いくつかの機能拡張

--- src/index.js
+++ src/index.js
@@ -3,44 +3,51 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
+const DIM = 4;
+const MARKS = { "1": "O", "-1": "X" };
+
 function Square(props) {
+  let className = "square";
+  if (props.mark) {
+    className += " mark";
+  }
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className={className} onClick={props.onClick}>
       {props.value}
     </button>
   );
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
+  renderSquare(i, isMark) {
     return (
       <Square
-        value={this.props.squares[i]}
+        key={"square-" + i}
+        mark={isMark}
+        value={MARKS[String(this.props.squares[i])]}
         onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+    let board = [];
+    let row = [];
+    for (let y = 0; y < DIM; y++) {
+      var cells = [];
+      for (let x = 0; x < DIM; x++) {
+        var seq = y * DIM + x;
+        cells.push(this.renderSquare(seq, this.props.markSeqs.includes(seq)));
+      }
+      row.push(
+        <div className="board-row" key={"row-" + y}>
+          {cells}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+      );
+    }
+    board.push(row);
+
+    return <div>{board}</div>;
   }
 }
 
@@ -50,11 +57,13 @@ class Game extends React.Component {
     this.state = {
       history: [
         {
-          squares: Array(9).fill(null)
+          squares: Array(DIM * DIM).fill(null),
+          lastHand: null
         }
       ],
       stepNumber: 0,
-      xIsNext: true
+      turn: 1,
+      isMarkLastHand: false
     };
   }
 
@@ -62,53 +71,100 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    const game_result = calculateWinner(squares);
+    if (game_result["winer"] || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? "X" : "O";
+
+    squares[i] = this.state.turn;
     this.setState({
       history: history.concat([
         {
-          squares: squares
+          squares: squares,
+          lastHand: seq_to_hand(i)
         }
       ]),
       stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
+      turn: -this.state.turn
     });
   }
 
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: step % 2 === 0
+      ignoreOnLeave: true,
+      isMarkLastHand: false,
+      turn: step % 2 === 0 ? 1 : -1
+    });
+  }
+
+  showHand(step) {
+    this.setState({
+      stepNumber: step,
+      ignoreOnLeave: false,
+      isMarkLastHand: true
+    });
+  }
+
+  showCurrent() {
+    let stepNumber;
+    if (this.state.ignoreOnLeave) {
+      stepNumber = this.state.stepNumber;
+    } else {
+      stepNumber = this.state.history.length - 1;
+    }
+
+    this.setState({
+      stepNumber: stepNumber,
+      ignoreOnLeave: false,
+      isMarkLastHand: false
     });
   }
 
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const game_result = calculateWinner(current.squares);
+    const winner = MARKS[String(game_result["winer"])];
 
     const moves = history.map((step, move) => {
-      const desc = move ? "Go to move #" + move : "Go to game start";
+      const label = move ? "[" + step.lastHand + "]" : "start";
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button
+            className="history-button"
+            onClick={() => this.jumpTo(move)}
+            onMouseEnter={() => this.showHand(move)}
+            onMouseLeave={() => this.showCurrent()}
+          >
+            {label}
+          </button>
         </li>
       );
     });
 
     let status;
-    if (winner) {
+    let markSeqs = [];
+    if (game_result["isDraw"]) {
+      status = "Draw:";
+    } else if (winner) {
       status = "Winner: " + winner;
+      markSeqs = game_result["line"];
     } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+      status = "Next player: " + MARKS[String(this.state.turn)];
+      if (this.state.isMarkLastHand && current.lastHand) {
+        markSeqs = [hand_to_seq(current.lastHand)];
+      }
     }
 
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+          <Board
+            squares={current.squares}
+            markSeqs={markSeqs}
+            onClick={i => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
           <div>{status}</div>
@@ -120,25 +176,57 @@ class Game extends React.Component {
 }
 
 // ========================================
-
 ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+  const lines = check_lines();
+  for (let line of lines) {
+    let winer = squares[line[0]];
+    if (!winer) {
+      continue;
+    }
+
+    for (let p of line) {
+      if (squares[p] !== winer) {
+        winer = null;
+        break;
+      }
+    }
+    if (winer) {
+      return { winer: winer, line: line, isDraw: false };
     }
   }
-  return null;
+  return { winer: null, line: [], isDraw: !squares.includes(null) };
+}
+
+function check_lines() {
+  let lines = [];
+  for (let i = 0; i < DIM; i++) {
+    // 横
+    let ls = [[], []];
+    for (let j = 0; j < DIM; j++) {
+      ls[0].push(i * DIM + j);
+      ls[1].push(i + DIM * j);
+    }
+    lines.push(ls[0].slice(0, DIM)); // 横
+    lines.push(ls[1].slice(0, DIM)); // 縦
+  }
+
+  // 対角線
+  let digs = [[], []];
+  for (let i = 0; i < DIM; i++) {
+    digs[0].push(i * DIM + i);
+    digs[1].push((i + 1) * DIM - i - 1);
+  }
+  lines.push(digs[0]);
+  lines.push(digs[1]);
+  return lines;
+}
+
+function hand_to_seq(hand) {
+  return (hand[0] - 1) * DIM + hand[1] - 1;
+}
+
+function seq_to_hand(seq) {
+  return [1 + Math.floor(seq / DIM), 1 + (seq % DIM)];
 }
